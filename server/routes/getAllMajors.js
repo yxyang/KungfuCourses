12a89d713be50a8689169a63ca957f2489a4791b
Dian Chen <dianchen96@gmail.com>
2016-02-11 21:10:37 -0800

    update KungfuCourses front-end

--- server/routes/getAllMajors.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-var path = require("path");
-
-module.exports = function(req, res, next) {
-	var map = require('../data/map.json');
-	res.jsonp(Object.keys(map));
-	next();
-}
--- server/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	getCourseByMajor: require('./getCourseByMajor'),
-	getCourseDetail: require('./getCourseDetail'),
-	getAllMajors: require('./getAllMajors')
-};
